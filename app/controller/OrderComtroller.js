b3405b011e1ec92ece4a077552b8b00ff7e4029c
User <User@LAPTOP-UIF87597>
2023-04-15 16:42:07 +0700

    getOrderone

--- app/controller/OrderComtroller.js
+++ app/controller/OrderComtroller.js
@@ -46,3 +46,57 @@ exports.createOrderDetail = (req,res) =>{
     console.log(err)
   }
 }
+exports.getAlloneOrders = (req,res) =>{
+  try{
+      db.query('Select * From orderdetail Where OrderId = ?', req.params.OrderId, (error, data)=>{
+          if (error) {
+              console.log(error);
+              res.status(500).json({ message: 'can not show Orderdetail' });
+          }else if (data.length ===0 ) {
+              res.status(401).json({ message: 'Invalid Orderdetail user' });
+          } else {
+              res.json(data);
+          }
+      });
+  }
+  catch(err){
+      console.log(err)
+  }
+}
+
+exports.getOneOrder = (req,res) =>{
+  try{
+      db.query('Select * From orders Where OrderId = ?', req.params.OrderId, (error, data)=>{
+          if (error) {
+              console.log(error);
+              res.status(500).json({ message: 'can not show Orderdetail' });
+          }else if (data.length ===0 ) {
+              res.status(401).json({ message: 'Invalid Orderdetail user' });
+          } else {
+              res.json(data[0]);
+          }
+      });
+  }
+  catch(err){
+      console.log(err)
+  }
+}
+
+exports.getAlluser = (req,res) =>{
+  try{
+      const {username} = req.body;
+      db.query('Select * From orders Where username = ?', [username], (error, data)=>{
+          if (error) {
+              console.log(error);
+              res.status(500).json({ message: 'can not show Order' });
+          }else if (data.length ===0 ) {
+              res.status(401).json({ message: 'Invalid order user' });
+          } else {
+              res.json(data);
+          }
+      });
+  }
+  catch(err){
+      console.log(err)
+  }
+}
--- app/routes/Routes.js
+++ app/routes/Routes.js
@@ -2,12 +2,19 @@ module.exports = app =>{
     const Product = require("../controller/ProCon.js");
     const Carts = require("../controller/CartController.js")
     const Orders = require("../controller/OrderComtroller.js")
+
     app.get("/products",Product.findAll);
     app.get("/products/:ProId",Product.findOne);
+
     app.post("/addCart",Carts.addCart);
     app.post("/carts",Carts.getAllProduct);
     app.delete("/carts/:CartId",Carts.DelOneCart);
     app.delete("/cartsDel/:username",Carts.DelCart);
+
+
     app.post("/order",Orders.createOrder);
     app.post("/orderdetail",Orders.createOrderDetail);
+    app.post("/getOrder",Orders.getAlluser);
+    app.get("/getorderdetail/:OrderId",Orders.getAlloneOrders)
+    app.get("/order/:OrderId",Orders.getOneOrder)
 }
